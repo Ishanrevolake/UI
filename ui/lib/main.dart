e6c74ebe680ce79699ecc5be49a10822cef8db50
Ishan579 <61306698+Ishan579@users.noreply.github.com>
2021-07-21 09:18:33 +0530

    Adding comments

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -2,14 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+/* Main method  */
 void main() {
   runApp(Homepage());
 }
 
+/* Homepage class is extended by statefulwidget class */
 class Homepage extends StatefulWidget {
   const Homepage({key}) : super(key: key);
 
   @override
+  /*   */
   _HomepageState createState() => _HomepageState();
 }
 
@@ -27,12 +30,15 @@ class _HomepageState extends State<Homepage> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white,
+        /* Ctreatong App bar */
         appBar: AppBar(
           shadowColor: Colors.transparent,
           title: Text(
             "Billing Detail",
             style: TextStyle(color: Colors.blue[900]),
           ),
+          /* Inserting icon buttons for app bar */
           leading: IconButton(
             icon: Icon(
               Icons.arrow_back_ios_outlined,
@@ -51,11 +57,16 @@ class _HomepageState extends State<Homepage> {
           ],
           backgroundColor: Colors.white,
         ),
+        /* Creating body  */
         body: Padding(
+          /* Padding for the whole body */
           padding: const EdgeInsets.all(10.0),
           child: Container(
             color: Colors.white70,
+            /* Adding a column for make the layout of the body */
             child: Column(
+              /* Creating containers under the column for insert 
+              the google maps and location details  */
               children: [
                 Container(
                     height: 160,
@@ -66,19 +77,26 @@ class _HomepageState extends State<Homepage> {
                           topLeft: Radius.circular(20),
                           topRight: Radius.circular(20)),
                     ),
+                    /*  Adding ClipRRect for make corners round in the map layout */
                     child: ClipRRect(
                       borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(20),
                         topRight: Radius.circular(20),
                       ),
+
+                      /* Adding Google maps */
                       child: GoogleMap(
+                        myLocationEnabled: true,
                         onMapCreated: _onMapCreated,
+                        zoomControlsEnabled: false,
                         initialCameraPosition: CameraPosition(
                           target: _center,
                           zoom: 11.0,
                         ),
                       ),
                     )),
+
+                /* Creating container for the location details  */
                 Container(
                   height: 70,
                   width: 300,
@@ -88,7 +106,9 @@ class _HomepageState extends State<Homepage> {
                         bottomLeft: Radius.circular(20),
                         bottomRight: Radius.circular(20)),
                   ),
+                  /* Using a Row to add the address line and the icon at the same line */
                   child: Row(
+                    /*Making the space childs of row horizontally  */
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Column(
@@ -110,7 +130,6 @@ class _HomepageState extends State<Homepage> {
                       Container(
                         width: 70,
                       ),
-
                       MaterialButton(
                         onPressed: () {},
                         color: Colors.yellow[600],
@@ -122,17 +141,11 @@ class _HomepageState extends State<Homepage> {
                         padding: EdgeInsets.all(12),
                         shape: CircleBorder(),
                       )
-                      // IconButton(
-                      //   icon: Icon(
-
-                      //     Icons.border_color,
-                      //     color: Colors.blue[900],
-                      //   ),
-                      //   onPressed: () {},
-                      // ),
                     ],
                   ),
                 ),
+
+                /* Creating layout for  payment optionss */
 
                 Padding(
                   padding: const EdgeInsets.all(10.0),
@@ -143,9 +156,6 @@ class _HomepageState extends State<Homepage> {
                         decoration: BoxDecoration(
                           color: Colors.grey[300],
                           borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            // BoxShadow(color: Colors.black12, blurRadius: 5)
-                          ],
                         ),
                         child: SizedBox(
                           width: 300,
@@ -154,12 +164,12 @@ class _HomepageState extends State<Homepage> {
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
                               Text(
-                                " Credit/Debit",
+                                " Credit/Debit Card",
                                 style: TextStyle(
                                     color: Colors.blue[800], fontSize: 20),
                               ),
                               Container(
-                                width: 50,
+                                width: 10,
                               ),
                               Radio(
                                   value: 1,
@@ -242,7 +252,7 @@ class _HomepageState extends State<Homepage> {
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
                               Text(
-                                " Google Play",
+                                " Google Pay",
                                 style: TextStyle(
                                     color: Colors.blue[800], fontSize: 20),
                               ),
@@ -264,21 +274,18 @@ class _HomepageState extends State<Homepage> {
                     ],
                   ),
                 ),
+                /* Layout for check out option */
                 Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Container(
                     decoration: BoxDecoration(
                       color: Colors.yellow[600],
                       borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        // BoxShadow(color: Colors.black12, blurRadius: 5)
-                      ],
                     ),
                     width: 300,
                     child: Row(
-                      // mainAxisAlignment: MainAxisAlignment.center,
-
                       children: [
+                        /*Adding a GestureDetector , Detecting the Gesture of the Widget as a button */
                         new GestureDetector(
                             onTap: () {
                               print("Heading to Check Out");
@@ -289,9 +296,6 @@ class _HomepageState extends State<Homepage> {
                               decoration: BoxDecoration(
                                 color: Colors.blue[900],
                                 borderRadius: BorderRadius.circular(20),
-                                boxShadow: [
-                                  // BoxShadow(color: Colors.black12, blurRadius: 5)
-                                ],
                               ),
                               child: Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
@@ -345,103 +349,8 @@ class _HomepageState extends State<Homepage> {
                       ],
                     ),
                   ),
-
-                  // child: Row(
-                  //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  //   children: [
-                  //     Container(
-                  //         height: 45,
-                  //         width: 300,
-                  //         decoration: BoxDecoration(
-                  //           color: Colors.grey[300],
-                  //           borderRadius: BorderRadius.circular(20),
-                  //         ))
-                  //   ],
-                  // ),
                 )
-
-                //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                //   children: [
-                //     Container(
-                //       decoration: BoxDecoration(
-                //         color: Colors.grey[300],
-                //         borderRadius: BorderRadius.circular(20),
-                //         // boxShadow: [
-                //         //   BoxShadow(color: Colors.black12, blurRadius: 5)
-                //         // ],
-                //       ),
-                //       child: SizedBox(
-                //         width: 300,
-                //         height: 65,
-                //         child: Row(
-                //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                //           children: [
-                //             Text(
-                //               " Google Play",
-                //               style: TextStyle(
-                //                   color: Colors.blue[800], fontSize: 20),
-                //             ),
-                //             Radio(
-                //                 value: 3,
-                //                 groupValue: _value,
-                //                 onChanged: (value) {
-                //                   setState(() {
-                //                     _value = value;
-                //                   });
-                //                 }),
-                //           ],
-                //         ),
-                //       ),
-                //     ),
-                //   ],
               ],
-
-              //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //   children: <Widget>[
-              //     Expanded(
-              //       flex: 3,
-              //       child: Container(
-              //         alignment: Alignment.center,
-              //         color: Colors.blue[200],
-              //         child: SizedBox(
-              //           height: 200,
-              //           child: Image.asset("assests/back.jpg"),
-              //         ),
-              //       ),
-              //     ),
-              //     Expanded(
-              //       flex: 2,
-              //       child: Container(
-              //         alignment: Alignment.center,
-              //         color: Colors.red,
-              //         child: Text("Middle"),
-              //       ),
-              //     ),
-              //     Expanded(
-              //       flex: 2,
-              //       child: Container(
-              //         alignment: Alignment.center,
-              //         color: Colors.green,
-              //         child: Text("Middle"),
-              //       ),
-              //     ),
-              //     Expanded(
-              //       flex: 2,
-              //       child: Container(
-              //         alignment: Alignment.center,
-              //         color: Colors.blue[100],
-              //         child: Text("Middle"),
-              //       ),
-              //     ),
-              //     Expanded(
-              //       flex: 1,
-              //       child: Container(
-              //         alignment: Alignment.center,
-              //         color: Colors.pink,
-              //         child: Text("last"),
-              //       ),
-              //     ),
-              //   ],
             ),
           ),
         ),
